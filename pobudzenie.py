fe9825aee9bd9dfd1468af4f7f75506a508b1ae9
Maciek <maciejwieczor@gmail.com>
2021-04-18 20:24:45 +0200

    Dodano triangle wave generator prototyp

--- pobudzenie.py
+++ pobudzenie.py
@@ -1,18 +1,38 @@
 class Pobudzenie:
-    def __init__(self, length, type, amplitude, square_width):
+    def __init__(self, length, type, amplitude, width):
         self.length = length
         self.type = type
         self.values = []
         self.amplitude = amplitude
-        self.square_width = square_width
+        self.width = width
         self.y = self.create_list_value()
 
     def create_list_value(self):
+
         if(self.type == "square"):
-            for i in range (0,self.square_width):
+            for i in range (0,self.width):
                 self.values.append(self.amplitude)
-            for i in range (self.square_width, self.length):
+            for i in range (self.width, self.length):
                 self.values.append(0)
+
+        if(self.type == "triangle"):
+            self.values.append(0)
+            loop_num = int(self.length / self.width) + 1
+            for i in range (0, loop_num):
+                if(self.width % 2 == 0):
+                    half = int((self.width)/2)
+                else:
+                    half = int((self.width - 1)/2)
+                for i in range(1, half+1):
+                    self.values.append(self.amplitude * i / (half + 1))
+                self.values.append(self.amplitude)
+                for i in range(-half, 0):
+                    self.values.append(self.amplitude * (-i) / (half + 1))
+                self.values.append(0)
+            reszta = len(self.values) - self.length
+            for i in range(0, reszta):
+                self.values.pop()
+
         return self.values
 
     def value_return(self):
--- program.py
+++ program.py
@@ -1,4 +1,13 @@
 from pobudzenie import Pobudzenie
+import numpy as np
+import matplotlib.pyplot as plt
 
-kwadrat = Pobudzenie(10, "square", 5, 2)
+range_var = 20
+kwadrat = Pobudzenie(range_var, "triangle", 3, 7)
+x = []
+for i in range(0, range_var):
+    x.append(i)
+y = kwadrat.value_return()
 print(kwadrat.value_return())
+plt.plot(x,y)
+plt.show()
