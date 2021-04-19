06c1470e6dcc12c34e1e3ef0f73e2994f06dd600
Maciek <maciejwieczor@gmail.com>
2021-04-19 08:12:25 +0200

    Dodano komentarze

--- pobudzenie.py
+++ pobudzenie.py
@@ -9,8 +9,8 @@ class Pobudzenie:
         self.y = self.create_list_value()
 
 
-    def phase(self):
-        if(self.phase_shift < 0):
+    def phase(self):                                                #phase shift - dla ujemnych int w prawo, dla dodatnich w lewo
+        if(self.phase_shift < 0):                                   #jeśli self.phase_shift = 0, zwraca niezmienioną listę
             for i in range(0, abs(self.phase_shift)):
                 self.values.insert(0, 0)
                 self.values.pop()
@@ -20,8 +20,8 @@ class Pobudzenie:
                 self.values.pop(0)
         return self.values
 
-    def create_list_value(self):
-
+    def create_list_value(self):                                    #tworzenie listy pobudzenia
+                                                                    #opcje : "square", "triangle"
         if(self.type == "square"):
             for i in range (0,self.width):
                 self.values.append(self.amplitude)
@@ -30,7 +30,7 @@ class Pobudzenie:
 
         if(self.type == "triangle"):
             self.values.append(0)
-            loop_num = int(self.length / self.width) + 1
+            loop_num = int(self.length / self.width) + 1                        #określenie ile pół okresów ma się wykonać + 1 który zostanie na końcu poprawiony
             for i in range (0, loop_num):
                 if(self.width % 2 == 0):
                     half = int((self.width)/2)
@@ -41,17 +41,15 @@ class Pobudzenie:
                 self.values.append(self.amplitude)
                 for i in range(-half, 0):
                     self.values.append(self.amplitude * (-i) / (half + 1))
-                self.values.append(0)
-            reszta = len(self.values) - self.length
+                self.values.append(0)                                           #zero na koniec każdego pół okresu
+            reszta = len(self.values) - self.length                             #dopełnienie ostatniego okresu
             for i in range(0, reszta):
                 self.values.pop()
 
         self.values = self.phase()
         return self.values
 
-
-
-    def value_return(self):
+    def value_return(self):                                                     #hermetyzacja czy coś
         return self.y
 
 
