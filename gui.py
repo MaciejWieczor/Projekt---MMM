7b56663de5ed38cbd8d9b323aa293448766391e7
Maciek <maciejwieczor@gmail.com>
2021-04-21 06:46:57 +0200

    Dodano plik i klasę GUI

---
+++ gui.py
@@ -0,0 +1,33 @@
+class GUI:
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#range_var = 3000
+#step = 0.01
+#sygnal1 = Pobudzenie(range_var, "square", 1, 2, step, 0)
+#sygnal2 = Odpowiedz_impulsowa(range_var, step, 1.3, 6.33, 3.62, 0, 0.7)
+#splot = Convolution(sygnal1.value_return(), sygnal2.value_return())
+#print(splot.value_return())
+#fig, axs = plt.subplots(3)
+#axs[0].plot(sygnal1.index_return(), sygnal1.value_return())
+#axs[1].plot(sygnal2.index_return(), sygnal2.value_return())
+#x = []
+#for i in range(0, len(splot.value_return())):
+#    x.append(i*step)
+#axs[2].plot(x,splot.value_return())
+#plt.show()
