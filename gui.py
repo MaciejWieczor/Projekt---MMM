0c889ecbda6747e621073b33e004cb7abe576d12
Maciek <maciejwieczor@gmail.com>
2021-04-30 20:27:24 +0200

    Dodałem rysowanie charakterystyk bodego do gui ale podejrzewam problemy z działaniem

--- gui.py
+++ gui.py
@@ -6,6 +6,7 @@ from pobudzenie import Pobudzenie
 from odpowiedz_impulsowa import Odpowiedz_impulsowa
 from splot import Convolution
 from rownanie_rozniczkowe import Rownanie_rozniczkowe
+from charakterystyki_bodego import Charakterystyki_bodego
 import numpy as np
 import math
 
@@ -40,16 +41,23 @@ class GUI:
             y.append(0)
         sygnal1 = Pobudzenie(range_var, type, amp, freq, step, phase) 
         sygnal2 = Rownanie_rozniczkowe(y, range_var, step, param[0], param[1], param[2], param[3], param[4], 0, 0, T)
+        charakterystyki = Charakterystyki_bodego(range_var, step, param[0], param[1], param[2], param[3], param[4], T)  #dodaj do rysowania na grid z boku
         #sygnal2 = Odpowiedz_impulsowa(range_var, step, param[0],param[1],param[2],param[3],param[4], T)
         splot = Rownanie_rozniczkowe(sygnal1.value_return(), range_var, step, param[0], param[1], param[2], param[3], param[4], 0, 0, T)
         x_pob = sygnal1.index_return()[:int(2*range_var/freq)+int(abs(phase/step))]
         y_pob = sygnal1.value_return()[:int(2*range_var/freq)+int(abs(phase/step))]
         fig = self.fig
-        gs = fig.add_gridspec(2,2, hspace=0.5, wspace=0.3)
+        gs = fig.add_gridspec(2,3, hspace=0.5, wspace=0.5)
         z = []
         for i in range(0, range_var):
             z.append(i*step)
         ax1 = fig.add_subplot(gs[0, 0])
+        ax4 = fig.add_subplot(gs[0, 2])
+        ax4.title.set_text("Charakterystyka amplitudowa odpowiedzi")
+        ax4.plot(charakterystyki.return_indexes_amplitude(), charakterystyki.return_values_amplitude())
+        ax5 = fig.add_subplot(gs[1, 2])
+        ax5.title.set_text("Charakterystyka częstotliwościowa odpowiedzi")
+        ax5.plot(charakterystyki.return_indexes_phase(), charakterystyki.return_values_phase())
         ax1.plot(x_pob, y_pob)
         ax1.title.set_text(f"Pobudzenie typu {type}")
         ax1.set_xlabel("czas (t = n * krok)")
@@ -59,11 +67,11 @@ class GUI:
         ax2.title.set_text("Odpowiedź impulsowa układu")
         ax2.set_xlabel("czas (t = n * krok)")
         ax2.set_ylabel("h[t]")
-        ax3 = fig.add_subplot(gs[1, :])
+        ax3 = fig.add_subplot(gs[1, :2])
         x = []
         for i in range(0, len(splot.value_return())):
             x.append(i*step)
         ax3.plot(x, splot.value_return())
-        ax3.title.set_text(f"Odpowiedź układu czyli równanie różniczkowe z pobudzeniem typu {type}")
+        ax3.title.set_text(f"Odpowiedź układu na pobudzenie typu {type}")
         ax3.set_xlabel("czas (t = n * krok)")
         ax3.set_ylabel("y[t]")
